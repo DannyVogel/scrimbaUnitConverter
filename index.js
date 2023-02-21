96d869dfe2445a539424138128912f132511060e
DannyVogel <danny.hj.vogel@gmail.com>
2023-02-21 16:19:02 +0100

    create metric/imperial unit converter with html and javascript

---
+++ index.js
@@ -0,0 +1,35 @@
+const lengthResultEl = document.getElementById("lengthResult")
+const volumeResultEl = document.getElementById("volumeResult")
+const massResultEl = document.getElementById("massResult")
+
+const convertBtn = document.getElementById("convertBtn")
+convertBtn.addEventListener("click", () => {
+    renderConversions()
+})
+
+function renderConversions(){
+    const inputVal = document.getElementById("inputEl").value
+    const conversions = convertAll(inputVal)
+    
+    lengthResultEl.innerText = `${inputVal} meters = ${conversions.length.imperial} feet | ${inputVal} feet = ${conversions.length.metric} meters` 
+    volumeResultEl.innerText = `${inputVal} liters = ${conversions.volume.imperial} gallons | ${inputVal} gallons = ${conversions.volume.metric} liters`
+    massResultEl.innerText = `${inputVal} kilos = ${conversions.mass.imperial} pounds | ${inputVal} pounds = ${conversions.mass.metric} kilos`
+}
+
+function convertAll(input) {
+    const lengthFactor = 3.281
+    const volumeFactor = 0.264
+    const massFactor = 2.204
+
+    const length = calculate(input, lengthFactor)
+    const volume = calculate(input, volumeFactor)
+    const mass = calculate(input, massFactor)
+
+    return ({length, volume, mass})
+}
+
+function calculate(num, factor) {
+    const imperial = (num * factor).toFixed(3)
+    const metric = (num / factor).toFixed(3)
+    return ({imperial, metric})
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+Simple metric/imperial unit converter created with HTML and Javascript (no styling). 
+
+Primarily used as a method to practice organizing code and separating it into small, reusable functions.
